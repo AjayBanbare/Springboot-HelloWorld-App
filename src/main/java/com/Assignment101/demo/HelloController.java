d4102b89fbf7bb1ea95c89fde899c412b067ff3b
AjayBanbare <ajaybanbare12@gmail.com>
2023-09-20 18:07:53 +0530

    first commit

---
+++ src/main/java/com/Assignment101/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.Assignment101.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
